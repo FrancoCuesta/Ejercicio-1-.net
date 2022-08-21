7868c78e6a5c10e80c6bc94916c33bc9d3222ab3
Lolellus <81826356+Lolellus@users.noreply.github.com>
2022-08-21 14:26:56 -0300

    listop

---
+++ P1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using shared;
+using System.Collections;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Mi Primera App");
+        List<Persona> list = new List<Persona>();
+        do
+        {
+            try
+            {
+                Console.WriteLine("Nueva Persona");
+                Persona p = new Persona();
+                Console.WriteLine("Nombre: ");
+                p.Nombre = Console.ReadLine();
+                Console.WriteLine("Documento: ");
+                p.Documento = Console.ReadLine();
+                Console.WriteLine("Fecha Nacimiento: ");
+                DateTime thisDate1 = Convert.ToDateTime(Console.ReadLine());
+                p.SetFechaNacimiento(thisDate1);
+                Console.WriteLine("Persona");
+                Console.WriteLine("Documento: " + p.Documento);
+                Console.WriteLine("Nombre: " + p.Nombre);
+                Console.WriteLine("Fnacimiento: " + p.GetFechaNacimiento());
+                list.Add(p);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR" + ex.Message);
+            }
+        } while (!Console.ReadLine().Equals("EXIT"));
+        Console.WriteLine("Personas");
+        Persona[] list2 = list.ToList().OrderBy(x => x.GetFechaNacimiento()).ToArray();
+        list2.ToList().ForEach(x => Console.WriteLine(x.Mostrar()));
+        Console.ReadLine();
+    }
+}
---
+++ shared/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared
+{
+    public class Persona
+    {
+        private DateTime fechaNacimiento;
+        public DateTime GetFechaNacimiento()
+        {
+            return fechaNacimiento;
+        }
+        public void SetFechaNacimiento(DateTime value)
+        {
+            fechaNacimiento = value;
+        }
+        public string Nombre { get; set; } = "--Sin Nombre--";
+        private string documento = "";
+        public string Documento
+        {
+            get { return documento; }
+            set
+            {
+                if (value.Length < 7)
+                    throw new Exception("Formato de documento incorrecto");
+                else
+                    documento = value.ToUpper();
+            }
+        }
+        public string Mostrar()
+        {
+            return "Nombre: " + Nombre + "\n Documento: "+Documento + "\n Naciminento: " + fechaNacimiento+ "\n";
+        }
+    }
+}
